fc19121fb8632ffacba35f3cf3397bf970e32ea3
konodop <150059168+konodop@users.noreply.github.com>
2024-10-27 16:02:30 +0300

    Update main.go
    Калькулятор который знает +, -, *, /, (   )  и  проверяет на ошибки

--- main.go
+++ main.go
@@ -1,99 +1,149 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 func Calc(expression string) (float64, error) {
-	var left, right int = 0, 0
-	var did int = 0
-	for _, g := range expression {
-		if did == 0 {
-			if g == '1' {
-				left *= 10
-				left += 1
-			} else if g == '2' {
-				left *= 10
-				left += 2
-			} else if g == '3' {
-				left *= 10
-				left += 3
-			} else if g == '4' {
-				left *= 10
-				left += 4
-			} else if g == '5' {
-				left *= 10
-				left += 5
-			} else if g == '6' {
-				left *= 10
-				left += 6
-			} else if g == '7' {
-				left *= 10
-				left += 7
-			} else if g == '8' {
-				left *= 10
-				left += 8
-			} else if g == '9' {
-				left *= 10
-				left += 9
-			} else if g == '0' {
-				left *= 10
-			} else if g == '+' {
-				did = 1
+	opn := 0
+	cls := 0
+	if len(expression) == 0 {
+		return 0, errors.New("Empty expression")
+	}
+	for i, j := range expression {
+		if j == '(' {
+			if i+1 < len(expression) {
+				if expression[i+1] == ')' {
+					return 0, errors.New("Syntax Error")
+				}
+			}
+			opn++
+		} else if j == ')' {
+			cls++
+		}
+		if opn < cls {
+			return 0, errors.New("Syntax Error")
+		}
+
+		if j == '+' || j == '*' || j == '/' {
+			if i == 0 || i == len(expression)-1 {
+				return 0, errors.New("Syntax Error")
+			} else if expression[i-1] == '(' || expression[i+1] == '+' || expression[i+1] == '*' || expression[i+1] == '/' || expression[i+1] == ')' {
+				return 0, errors.New("Syntax Error")
+			}
+
+		} else if j == '-' {
+			if i == len(expression)-1 {
+				return 0, errors.New("Syntax Error")
+			} else if expression[i+1] == '-' || expression[i+1] == '+' || expression[i+1] == '*' || expression[i+1] == '/' || expression[i+1] == ')' {
+				return 0, errors.New("Syntax Error")
+			}
+		}
+	}
+
+	if opn != cls {
+		return 0, errors.New("Syntax Error")
+	}
+	for {
+		var s int = 0
+		for i := range len(expression) {
+			if expression[i] == '(' {
+				s = i + 1
+			}
+		}
+		var q rune = ' '
+		i := s
+		index := 0
+		for i < len(expression) && expression[i] != ')' {
+			if expression[i] == '*' {
+				index = i
+				q = '*'
+				break
+			} else if expression[i] == '/' {
+				index = i
+				q = '/'
+				break
+			} else if expression[i] == '+' && q == ' ' {
+				q = '+'
+				index = i
+			} else if expression[i] == '-' && q == ' ' {
+				q = '-'
+				index = i
+			}
+			i++
+		}
+		if q == ' ' {
+			if s != 0 {
+				if expression[s-1] == '(' && i < len(expression) {
+					expression = expression[:s-1] + expression[s:i] + expression[i+1:]
+					continue
+				}
+			}
+			break
+		}
+		right := ""
+		left := ""
+		i = index - 1
+		for i >= 0 {
+			g := expression[i]
+			if g == '(' {
+				break
+			} else if g == '+' || g == '*' || g == '/' {
+				break
 			} else if g == '-' {
-				did = 2
-			} else if g == '*' {
-				did = 3
-			} else if g == '/' {
-				did = 4
+				if i == s {
+					left = "-" + left
+					i--
+				} else if expression[i-1] == '-' || expression[i-1] == '+' || expression[i-1] == '*' || expression[i-1] == '/' {
+					left = "-" + left
+					i--
+				}
+				break
 			}
+			left = expression[i:i+1] + left
+			i--
+		}
+		lt := i + 1
+		i = index + 1
+		for i < len(expression) {
+			g := expression[i]
+			if g == ')' {
+				break
+			} else if g == '+' || g == '*' || g == '/' {
+				break
+			} else if g == '-' {
+				if expression[i-1] == '-' || expression[i-1] == '+' || expression[i-1] == '*' || expression[i-1] == '/' {
+				} else {
+					break
+				}
+			}
+			right = right + expression[i:i+1]
+			i++
+		}
+		rt := i
+		ans := ""
+		lefted, _ := strconv.ParseFloat(left, 64)
+		righted, _ := strconv.ParseFloat(right, 64)
+		if q == '*' {
+			ans = strconv.FormatFloat(lefted*righted, 'f', 6, 64)
+		} else if q == '/' {
+			if righted == 0 {
+				return 0, errors.New("Сannot be divided by zero")
+			}
+			ans = strconv.FormatFloat(lefted/righted, 'f', 6, 64)
+		} else if q == '-' {
+			ans = strconv.FormatFloat(lefted-righted, 'f', 6, 64)
 		} else {
-			if g == '1' {
-				right *= 10
-				right += 1
-			} else if g == '2' {
-				right *= 10
-				right += 2
-			} else if g == '3' {
-				right *= 10
-				right += 3
-			} else if g == '4' {
-				right *= 10
-				right += 4
-			} else if g == '5' {
-				right *= 10
-				right += 5
-			} else if g == '6' {
-				right *= 10
-				right += 6
-			} else if g == '7' {
-				right *= 10
-				right += 7
-			} else if g == '8' {
-				right *= 10
-				right += 8
-			} else if g == '9' {
-				right *= 10
-				right += 9
-			} else if g == '0' {
-				right *= 10
-			}
+			ans = strconv.FormatFloat(lefted+righted, 'f', 6, 64)
 		}
+		expression = expression[:lt] + ans + expression[rt:]
 	}
-	if did == 1 {
-		return float64(left) + float64(right), nil
-	} else if did == 2 {
-		return float64(left) - float64(right), nil
-	} else if did == 3 {
-		return float64(left) * float64(right), nil
-	} else {
-		if right == 0 {
-			return 0.0, fmt.Errorf("division by zero")
-		}
-		return float64(left) / float64(right), nil
-	}
+	ex, _ := strconv.ParseFloat(expression, 64)
+	return ex, nil
 }
 
 func main() {
-	fmt.Println(Calc("4123 + 1234"))
+	fmt.Println(Calc("(1 + 1) * 2"))
 }
